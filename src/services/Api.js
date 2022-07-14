faefd9175279f901393bff045de3211fca719d58
aman-pen <aman.sharma@penthara.com>
2022-07-15 02:02:11 +0530

    added api file

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,5 +1,5 @@
-export function Api() {
-  return fetch("https://sessionize.com/api/v2/msqv8bxy/view/All").then((data) =>
-    data.json()
+export function ApiSpeaker() {
+  return fetch("https://sessionize.com/api/v2/yh2xd00c/view/Speakers").then(
+    (data) => data.json()
   );
 }
