4db808284274911d0b2c55be1ec11aa2e35c4187
aman-pen <aman.sharma@penthara.com>
2022-07-18 20:41:49 +0530

    SCRAPP EDIT

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useInsertionEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.scss";
 import { Header } from "./Header";
 import { About } from "./About";
@@ -11,8 +11,18 @@ import { Footer } from "./Footer";
 // import { KeyNoteSpeaker } from "./KeyNoteSpeaker";
 import { CountDownTimer } from "./CountDownTimer";
 import { EventTeam } from "./EventTeam";
-
+import { webScrap } from "./services/Api";
+import rp from "request-promise";
 function App() {
+  useEffect(() => {
+    rp({
+      url: "https://sessionize.com/global-azure-blr-2022/",
+      headers: {
+        "User-Agent": "Request-Promise",
+      },
+    }).then((html) => console.log(html));
+    // .catch((err) => console.log(err));
+  }, []);
   return (
     <div className="container-fluid icss">
       <Header />
--- src/services/Api.js
+++ src/services/Api.js
@@ -8,3 +8,17 @@ export function ApiSession() {
     (data) => data.json()
   );
 }
+
+// export function webScrap() {
+//   return fetch("https://sessionize.com/global-azure-blr-2022/")
+//     .then((data) => data.text())
+//     .then((data) => {
+//       console.log(data);
+//     });
+// }
+
+export async function webScrap() {
+  const response = await fetch("https://sessionize.com/global-azure-blr-2022/");
+  const text = await response.text();
+  console.log(text.match(/(?<=\<h1>).*(?=\<\/h1>)/));
+}
